bce657cb28fb379e0a19ee49fbcf389d647f1162
joeflack4 <joeflack4@gmail.com>
2016-03-12 15:06:34 -0600

    Updated manage.py

--- manage.py
+++ manage.py
@@ -5,11 +5,12 @@ from flask.ext.migrate import Migrate, MigrateCommand
 from app import app, db
 # app.config.from_object(os.environ['APP_SETTINGS'])
 
-
 migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
+    print("")
+    print("# # # Beginning Migration. DB = ", app.config['SQLALCHEMY_DATABASE_URI'], "# # #")
     manager.run()

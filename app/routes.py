a52e986d6ca4d7a8ef4bfe7f83fc5ccd7c915022
joeflack4 <joeflack4@gmail.com>
2016-03-27 17:33:31 -0500

    Updated routes and templates for to obey sessions.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, url_for, flash, redirect, request, session
+from functools import wraps
 from app import app
 # Unused -> from flask_table import Table, Col
 
@@ -33,65 +34,75 @@ except:
 
 ##############
 # - Variables
-logged_in = False
+
+
+##############
+# - Decorators
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Please log in before proceeding.', 'warning')
+            return redirect(url_for('login'))
+    return wrap
 
 
 ##############
 # - Root Path
 @app.route('/')
-@app.route('/home')
-@app.route('/index')
-@app.route('/dashboard')
-def index(logged_in=True):
-# def home(logged_in=True):
-    username = ""
-    if logged_in == False:
-        return redirect(url_for('login'))
+def root_path():
+    if session['logged_in']:
+        return redirect(url_for('index'))
     else:
-        return render_template('core_modules/dashboard/index.html',
-                           module_name="Just-a-Dash Control Panel",
-                           page_name="Dashboard",
-                           icon="fa fa-dashboard",
-                           logged_in=logged_in,
-                           username=username)
+        return redirect(url_for('welcome'))
 
 
 @app.route('/welcome')
 def welcome():
-    # username = ""
     return render_template('core_modules/welcome/index.html',
                            module_name="Just-a-Dash Control Panel",
                            page_name="Welcome",
-                           icon="fa fa-dashboard")
-                           # logged_in=logged_in,
-                           # username=username)
+                           icon="fa fa-star-o",
+                           module_abbreviation="Home",)
+
+
+@app.route('/index')
+@app.route('/dashboard')
+@app.route('/home')
+@login_required
+def index():
+    username = ""
+
+    return render_template('core_modules/dashboard/index.html',
+                           module_name="Just-a-Dash Control Panel",
+                           page_name="Dashboard",
+                           icon="fa fa-dashboard",
+                           module_abbreviation="Home",
+                           username=username)
+
 
 ################
 # - Core Modules
 @app.route('/account-settings')
-def account_settings(logged_in=logged_in):
-    if not logged_in:
-        return redirect(url_for('login'))
-    else:
-        return render_template('core_modules/account_settings/index.html',
-                               icon="fa fa-dashboard",
-                               module_abbreviation="Account Settings",
-                               module_name="Account Settings",
-                               page_name="Account Settings Home",
-                               logged_in=logged_in)
+@login_required
+def account_settings():
+    return render_template('core_modules/account_settings/index.html',
+                           icon="fa fa-dashboard",
+                           module_abbreviation="Account Settings",
+                           module_name="Account Settings",
+                           page_name="Account Settings Home")
 
 
 @app.route('/app-settings')
-def app_settings(logged_in=logged_in):
-    if not logged_in:
-        return redirect(url_for('login'))
-    else:
-        return render_template('core_modules/app_settings/index.html',
-                               icon="fa fa-dashboard",
-                               module_abbreviation="App Settings",
-                               module_name="App Settings",
-                               page_name="App Settings Home",
-                               logged_in=logged_in)
+@login_required
+def app_settings():
+    return render_template('core_modules/app_settings/index.html',
+                           icon="fa fa-dashboard",
+                           module_abbreviation="App Settings",
+                           module_name="App Settings",
+                           page_name="App Settings Home")
 
 
 @app.route('/logout')
@@ -102,7 +113,7 @@ def logout():
 
 
 @app.route('/login', methods=['GET', 'POST'])
-def login(logged_in=logged_in):
+def login():
     # form = LoginForm()
     errors = []
 
@@ -113,18 +124,17 @@ def login(logged_in=logged_in):
                 flash(error, 'danger')
             return render_template('core_modules/login/index.html',
                                    icon="fa fa-dashboard",
-                                   module_abbreviation="ACP",
+                                   module_abbreviation="Home",
                                    module_name="Just-a-Dash Control Panel",
-                                   page_name="Login",
-                                   logged_in=logged_in)
+                                   page_name="Login")
                                    # errors=errors)
                                    # login_form=form)
-
         ## - This is from Flask Mega Tutorial, with OpenID
         # elif form.validate_on_submit():
         #     flash('Login requested for "%s", remember_me="%s"' %
         #           form.openid.data, str(form.remember_me.data))
         #     return redirect('/login')
+
         else:
             session['logged_in'] = True
             flash(u'Logged in. Welcome back!', 'success')
@@ -132,216 +142,193 @@ def login(logged_in=logged_in):
     else:
         return render_template('core_modules/login/index.html',
                                    icon="fa fa-dashboard",
-                                   module_abbreviation="ACP",
+                                   module_abbreviation="Home",
                                    module_name="Just-a-Dash Control Panel",
-                                   page_name="Login",
-                                   logged_in=logged_in)
-                                   # login_form=form)
+                                   page_name="Login")
 
 
 @app.route('/register', methods=['GET', 'POST'])
-def register(logged_in=False):
-    return "Thank you for your submission. The administrator will contact you when registration is complete." \
-           "<br/><br/><- <a href='/'>Go Back</>"
-
+def register():
+    return render_template('core_modules/register/index.html',
+                           icon="fa fa-pencil-square-o",
+                           module_abbreviation="Registration",
+                           module_name="Registration",
+                           page_name="New Submission")
 
 @app.route('/profile')
-def profile(logged_in=logged_in):
-    if not logged_in:
-        return redirect(url_for('login'))
-    else:
-        return render_template('core_modules/profile/index.html',
-                               icon="fa fa-dashboard",
-                               module_abbreviation="Profile",
-                               module_name="Profile",
-                               page_name="Profile Home",
-                               logged_in=logged_in)
+@login_required
+def profile():
+    return render_template('core_modules/profile/index.html',
+                           icon="fa fa-dashboard",
+                           module_abbreviation="Profile",
+                           module_name="Profile",
+                           page_name="Profile Home")
 
 
 ############
 # - Modules
 @app.route('/hr')
 @app.route('/hrm')
-def hrm(logged_in=logged_in):
-    if not logged_in:
-        return redirect(url_for('login'))
-    else:
-        try:
-            personnel = CompanyContacts.get_contacts()
-        except:
-            personnel = CompanyContacts.get_contacts()
-            # personnel = {"-": {"timestamp": "-", "first_name": "-", "last_name": "-", "phone_number": "-"}}
+@login_required
+def hrm():
+    try:
+        personnel = CompanyContacts.get_contacts()
+    except:
+        personnel = CompanyContacts.get_contacts()
+        # personnel = {"-": {"timestamp": "-", "first_name": "-", "last_name": "-", "phone_number": "-"}}
 
-        return render_template('modules/hrm/index.html',
+    return render_template('modules/hrm/index.html',
                                icon="fa fa-users",
                                module_abbreviation="HRM",
                                module_name="Human Resource Management",
                                page_name="HRM Home",
                                form_title="Personnel",
-                               logged_in=logged_in,
                                personnel_data=personnel)
 
 
 @app.route('/crm')
-def crm(logged_in=logged_in):
-    if not logged_in:
-        return redirect(url_for('login'))
-    else:
-        try:
-            customers = CompanyContacts.get_customer_contacts()
-        except:
-            customers = {"-": {"timestamp": "-", "first_name": "-", "last_name": "-", "phone_number": "-"}}
+@login_required
+def crm():
+    try:
+        customers = CompanyContacts.get_customer_contacts()
+    except:
+        customers = {"-": {"timestamp": "-", "first_name": "-", "last_name": "-", "phone_number": "-"}}
 
-        return render_template('modules/crm/index.html',
-                               icon="ion-person-stalker",
-                               module_abbreviation="CRM",
-                               module_name="Customer Relationship Management",
-                               page_name="CRM Home",
-                               form_title="Customer",
-                               logged_in=logged_in,
-                               customer_data=customers)
+    return render_template('modules/crm/index.html',
+                           icon="ion-person-stalker",
+                           module_abbreviation="CRM",
+                           module_name="Customer Relationship Management",
+                           page_name="CRM Home",
+                           form_title="Customer",
+                           customer_data=customers)
+
 
 
 @app.route('/operations')
-def operations(*args, logged_in=logged_in):
-    if not logged_in:
-        return redirect(url_for('login'))
+@login_required
+def operations(*args):
+    try:
+        check_in_type = args[0]
+    except:
+        check_in_type = None
+
+    if check_in_type == "sms_check_in":
+        check_in_entries = sms_check_in_data()
+    elif check_in_type == "call_check_in":
+        check_in_entries = call_check_in_data()
+    elif check_in_type == None:
+        check_in_entries = call_check_in_data()
     else:
-        try:
-            check_in_type = args[0]
-        except:
-            check_in_type = None
+        check_in_entries = {".": {"timestamp": ".", "first_name": ".", "last_name": ".", "phone_number": "."}}
 
-        if check_in_type == "sms_check_in":
-            check_in_entries = sms_check_in_data()
-        elif check_in_type == "call_check_in":
-            check_in_entries = call_check_in_data()
-        elif check_in_type == None:
-            check_in_entries = call_check_in_data()
-        else:
-            check_in_entries = {".": {"timestamp": ".", "first_name": ".", "last_name": ".", "phone_number": "."}}
-
-        return render_template('modules/operations/index.html',
-                               icon="fa fa-fort-awesome",
-                               module_abbreviation="OMS",
-                               module_name="Operations Management",
-                               page_name="OMS Home",
-                               logged_in=logged_in,
-                               check_in_entries=check_in_entries)
+    return render_template('modules/operations/index.html',
+                           icon="fa fa-fort-awesome",
+                           module_abbreviation="OMS",
+                           module_name="Operations Management",
+                           page_name="OMS Home",
+                           check_in_entries=check_in_entries)
 
 
 @app.route('/checkin')
 @app.route('/check-in')
 @app.route('/callin')
 @app.route('/call-in')
-def call_check_in(logged_in=logged_in):
-    if not logged_in:
-        return redirect(url_for('login'))
-    else:
-        return operations("call_check_in",
-                           logged_in=logged_in,)
+@login_required
+def call_check_in():
+    return operations("call_check_in")
 
 
 @app.route('/textin')
 @app.route('/text-in')
 @app.route('/text-checkin')
 @app.route('/sms-checkin')
-def sms_check_in(logged_in=logged_in):
-    if not logged_in:
-        return redirect(url_for('login'))
-    else:
-        return operations("sms_check_in",
-                           logged_in=logged_in,)
-
+@login_required
+def sms_check_in():
+    return operations("sms_check_in")
 
 
 @app.route('/bms')
 @app.route('/billing')
 @app.route('/ams')
 @app.route('/accounting')
-def accounting(logged_in=logged_in):
-    if not logged_in:
-        return redirect(url_for('login'))
-    else:
-        return render_template('modules/accounting/index.html',
-                               icon="fa fa-bar-chart",
-                               module_abbreviation="AMS",
-                               module_name="Accounting Management",
-                               page_name="AMS Home",
-                               logged_in=logged_in,)
+@login_required
+def accounting():
+    return render_template('modules/accounting/index.html',
+                           icon="fa fa-bar-chart",
+                           module_abbreviation="AMS",
+                           module_name="Accounting Management",
+                           page_name="AMS Home")
+
 
 
 @app.route('/mms', methods=['GET', 'POST'])
 @app.route('/marketing', methods=['GET', 'POST'])
-def marketing(logged_in=True):
-    if not logged_in:
-        return redirect(url_for('login'))
-    else:
-        errors = []
-        results = {}
-        if request.method == "POST":
+@login_required
+def marketing():
+    errors = []
+    results = {}
+    if request.method == "POST":
+        try:
+            url = request.form['url']
+            # See if URL submitted contains 'http://' prepended.
+            if url.find("http://") == 0:
+                # r = requests.get(url).text.encode("utf-8")
+                # r = requests.get(url).text
+                r = requests.get(url)
+                # print(r)
+            else:
+                url = "http://" + url
+                r = requests.get(url)
+        except:
+            errors.append('Unable to get URL. Please make sure it\'s valid and try again.')
+            return render_template('modules/marketing/index.html',
+                                   icon="fa fa-line-chart",
+                                   module_abbreviation="MMS",
+                                   module_name="Marketing Management",
+                                   page_name="MMS Home",
+                                   errors=errors)
+        if r:
+            # text processing
+            raw = BeautifulSoup(r.text).get_text()
+            nltk.data.path.append('./nltk_data/')  # set the path
+            tokens = nltk.word_tokenize(raw)
+            text = nltk.Text(tokens)
+
+            # remove punctuation, count raw words
+            nonPunct = re.compile('.*[A-Za-z].*')
+            raw_words = [w for w in text if nonPunct.match(w)]
+            raw_word_count = Counter(raw_words)
+
+            # stop words
+            no_stop_words = [w for w in raw_words if w.lower() not in stops]
+            no_stop_words_count = Counter(no_stop_words)
+
+            # save the results
+            results = sorted(
+                no_stop_words_count.items(),
+                key=operator.itemgetter(1),
+                reverse=True
+            )[0:10]
+
             try:
-                url = request.form['url']
-                # See if URL submitted contains 'http://' prepended.
-                if url.find("http://") == 0:
-                    # r = requests.get(url).text.encode("utf-8")
-                    # r = requests.get(url).text
-                    r = requests.get(url)
-                    # print(r)
-                else:
-                    url = "http://" + url
-                    r = requests.get(url)
+                result = Result(
+                    url=url,
+                    result_all=raw_word_count,
+                    result_no_stop_words=no_stop_words_count
+                )
+                db.session.add(result)
+                db.session.commit()
             except:
-                errors.append('Unable to get URL. Please make sure it\'s valid and try again.')
-                return render_template('modules/marketing/index.html',
-                       icon="fa fa-line-chart",
-                       module_abbreviation="MMS",
-                       module_name="Marketing Management",
-                       page_name="MMS Home",
-                       logged_in=logged_in,
-                       errors=errors)
-            if r:
-                # text processing
-                raw = BeautifulSoup(r.text).get_text()
-                nltk.data.path.append('./nltk_data/')  # set the path
-                tokens = nltk.word_tokenize(raw)
-                text = nltk.Text(tokens)
+                errors.append("Unable to add item to database.")
 
-                # remove punctuation, count raw words
-                nonPunct = re.compile('.*[A-Za-z].*')
-                raw_words = [w for w in text if nonPunct.match(w)]
-                raw_word_count = Counter(raw_words)
+    return render_template('modules/marketing/index.html',
+                           icon="fa fa-line-chart",
+                           module_abbreviation="MMS",
+                           module_name="Marketing Management",
+                           page_name="MMS Home",
+                           errors=errors,
+                           results=results)
 
-                # stop words
-                no_stop_words = [w for w in raw_words if w.lower() not in stops]
-                no_stop_words_count = Counter(no_stop_words)
-
-                # save the results
-                results = sorted(
-                    no_stop_words_count.items(),
-                    key=operator.itemgetter(1),
-                    reverse=True
-                )[0:10]
-
-                try:
-                    result = Result(
-                        url=url,
-                        result_all=raw_word_count,
-                        result_no_stop_words=no_stop_words_count
-                    )
-                    db.session.add(result)
-                    db.session.commit()
-                except:
-                    errors.append("Unable to add item to database.")
-
-        return render_template('modules/marketing/index.html',
-                               icon="fa fa-line-chart",
-                               module_abbreviation="MMS",
-                               module_name="Marketing Management",
-                               page_name="MMS Home",
-                               logged_in=logged_in,
-                               errors=errors,
-                               results=results)
 
 
 # - Services
@@ -361,4 +348,4 @@ def call():
 
 
 if __name__ == "__main__":
-    print("yolo")
+    print("## Running Just-a-Dash routes.py directly. ##")

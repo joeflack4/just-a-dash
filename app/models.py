3ca9161f33f807f0cf1232263d5847bb9ec46952
joeflack4 <joeflack4@gmail.com>
2016-03-27 18:52:01 -0500

    Added 'Messages' table. Added db_create.py

--- app/models.py
+++ app/models.py
@@ -17,3 +17,34 @@ class Result(db.Model):
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
+
+
+class Messages(db.Model):
+    __tablename__ = 'messages'
+
+    id = db.Column(db.Integer, primary_key=True)
+    # Type examples: UserMessages, Notifications, Tasks, etc.
+    type = db.Column(db.String())
+    # Subcategory examples: News, updates, etc.
+    subcategory = db.Column(db.String())
+    title = db.Column(db.String())
+    body = db.Column(db.String())
+    author = db.Column(db.String())
+    # Destinations examples: To users, groups.
+    destinations = db.Column(db.String())
+    # Delivery Method examples: To webapp, native app, push notification, SMS, e-mail, phone call, etc.
+    delivery_methods = db.Column(db.String())
+    notes = db.Column(db.String())
+
+    def __init__(self, type, subcategory, title, body, author, destinations, delivery_methods, notes):
+        self.type = type
+        self.subcategory = subcategory
+        self.title = title
+        self.body = body
+        self.author = author
+        self.destinations = destinations
+        self.delivery_methods = delivery_methods
+        self.notes = notes
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
---
+++ db_create.py
@@ -0,0 +1,18 @@
+from app import db
+from app.models import *
+
+db.create_all()
+
+# Initializes the database creation with a first message.
+# Messages Fields: type, subcategory, title, body, author, destinations, delivery_methods, notes.
+db.session.add(Messages("Notification",
+                        "Tutorial",
+                        "Welcome to Just-a-Dash!",
+                        "Welcome to Just-a-Dash, the minimalist's dashboard application. I hope that you enjoy your experience. To get in touch with me for comment / question / feature request / whatever it may be, feel free to e-mail joeflack4@gmail.com, or check out my blog, joeflack.net. - Joe Flack, Creator of Just-a-Dash",
+                        "Joe Flack",
+                        "Group:AllUsers",
+                        "WebApp, NativeApps, Email",
+                        ""
+                        ))
+
+db.session.commit()

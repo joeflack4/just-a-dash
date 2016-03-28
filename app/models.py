2c66a9e4eff42c6cfc6af147773265fe08c6bec0
joeflack4 <joeflack4@gmail.com>
2016-03-28 01:52:13 -0500

    Added relational mapping between User and Messages tables.

--- app/models.py
+++ app/models.py
@@ -2,17 +2,31 @@ from app import db, bcrypt
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import JSON
+from sqlalchemy.ext.declarative import declarative_base
+
+db.Base = declarative_base()
+
+user_messages = db.Table('user_messages', db.Base.metadata,
+    db.Column('user_id', db.Integer, ForeignKey('user.id')),
+    db.Column('messages_id', db.Integer, ForeignKey('messages.id'))
+)
 
 
 class User(db.Model):
+    __tablename__ = 'user'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
     groups = db.Column(db.String(500))
     permissions = db.Column(db.String(500))
-    sent_messages = relationship("Messages", backref="author")
-    received_messages = relationship("Messages", backref="destinations")
+    sent_messages = relationship("Messages", backref="user")
+    # received_messages = relationship("Messages", backref="user")
+    received_messages = relationship(
+        "messages",
+        secondary=user_messages,
+        back_populates="user")
 
     def __init__(self, username, email, password):
         self.username = username
@@ -34,11 +48,16 @@ class Messages(db.Model):
     title = db.Column(db.String())
     body = db.Column(db.String())
     author = db.Column(db.String())
-    destinations = db.Column(db.String())
-    # Destinations examples: To users, groups.
     delivery_methods = db.Column(db.String())
     # Delivery Method examples: To webapp, native app, push notification, SMS, e-mail, phone call, etc.
     notes = db.Column(db.String())
+    user_id = db.Column(db.Integer, ForeignKey('user.id'))
+    # destinations = db.Column(db.String())
+    # Destinations examples: To users, groups.
+    destinations = relationship(
+        "user",
+        secondary=user_messages,
+        back_populates="messages")
 
     def __init__(self, type, subcategory, title, body, author, destinations, delivery_methods, notes):
         self.type = type

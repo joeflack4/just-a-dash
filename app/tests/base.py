dbc77c1faba46b4d61eeeb59f77868acd09316c0
joeflack4 <joeflack4@gmail.com>
2016-05-11 01:44:03 -0500

    Updated comments.

--- app/tests/base.py
+++ app/tests/base.py
@@ -1,23 +1,37 @@
-from flask.ext.testing import TestCase
-
-from app import app, db
+from app import app
 from app.models import *
+# from flask.ext.testing import TestCase as t
+# import pytest
+import unittest
 
 
-class BaseTestCase(TestCase):
-    """A base test case."""
+# - Note: Testing implemented in flask testing extension; expirmentation.
+# class BaseTestCase(t):
+#     """A base test case."""
+#
+#     def create_app(self):
+#         app.config.from_object('config.TestConfig')
+#         return app
+#
+#     def setUp(self):
+#         db.create_all()
+#         # db.session.add(User("admin", "ad@min.com", "admin"))
+#         # db.session.add(
+#         #     BlogPost("Test post", "This is a test. Only a test.", "admin"))
+#         db.session.commit()
+#
+#     def tearDown(self):
+#         db.session.remove()
+#         db.drop_all()
 
-    def create_app(self):
-        app.config.from_object('config.TestConfig')
-        return app
 
-    def setUp(self):
-        db.create_all()
-        # db.session.add(User("admin", "ad@min.com", "admin"))
-        # db.session.add(
-        #     BlogPost("Test post", "This is a test. Only a test.", "admin"))
-        db.session.commit()
+# - Note: Testing implemented in pytest; expirimentation.
+# def f():
+#     raise SystemExit(1)
+#
+# def test_mytest():
+#     with pytest.raises(SystemExit):
+#         f()
 
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+
+# - Note: Testing implemented in unittest.

cd03de64e6ea147fc07c460ac30e53bf547fa2b3
joeflack4 <joeflack4@gmail.com>
2016-05-15 08:56:27 -0500

    Fixed 'Internal Server Error' bug related to app preloading.

--- app/config/gunicorn_config.py
+++ app/config/gunicorn_config.py
@@ -1,6 +1,14 @@
 # - Note: The reason for making this file is that I was trying to solve the following error:
 # sqlalchemy.exc.OperationalError: (OperationalError) SSL error: decryption failed or bad record mac
-# Heroku describes the problem further - https://devcenter.heroku.com/articles/forked-pg-connections
+# - More Notes:
+# * The Problem - https://devcenter.heroku.com/articles/forked-pg-connections
+# * Etc - http://stackoverflow.com/questions/36190763/celery-flask-sqlalchemy-databaseerror-databaseerror-ssl-error-decryption-f
+# * Gunicorn Config - http://docs.gunicorn.org/en/stable/settings.html#config-file
+# * More on Gunicorn Config - http://docs.gunicorn.org/en/stable/configure.html
+# * Post Fork - http://docs.gunicorn.org/en/stable/settings.html#post-fork
+# * Example to fix code - https://devcenter.heroku.com/articles/forked-pg-connections#unicorn-server
+# - Solution: Rather than the more optimized, more complicated solution of keeping pre-loading, and simply closing
+# db connection on pre-fork, etc, I simply turned off app pre-loading. This is not too bad if the app is small.
 
 # - Note: Example code taken from - http://smellman.hatenablog.com/entry/2013/04/17/022148
 # # -*- coding: utf-8 -*-

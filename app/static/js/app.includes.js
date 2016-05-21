3fd89785581efdee0c74de6240b6d56fc08e4f1b
joeflack4 <joeflack4@gmail.com>
2016-05-21 04:55:47 -0500

    Added proprietary app CSS & JS.

--- app/static/js/app.includes.js
+++ app/static/js/app.includes.js
@@ -1,3 +1,38 @@
 /**
  * Created by Joe Flack on 5/21/2016.
  */
+
+// (function(){
+$(document).ready(function() {
+
+    /////////////////
+    // Definitions //
+    /////////////////
+    // Gallery Module //
+    var fontIconAutoSizer = function(){
+        var fontSize = parseInt($(".gallery-container").width())+"px";
+        // $(".gallery-container span").css('font-size', fontSize);
+        $(".gallery-container i").css('font-size', fontSize);
+    };
+
+    /////////////
+    // Runtime //
+    /////////////
+    fontIconAutoSizer();
+
+    /////////////////////
+    // Event Listeners //
+    /////////////////////
+    // Gallery Module //
+    $(window).resize(fontIconAutoSizer);
+
+    // - Note: The below code is simply reference. I had a bit of a difficult time messing with selectors due to my poor
+    // JS syntax understanding at the time.
+    // $(window).resize(function(){
+    //     fontIconAutoSizer
+    // });
+    // $(window).resize(function(){alert('hi')});
+    // $('#red-icon')[0].onclick = fontIconAutoSizer;
+    // $('#red-icon')[0].onclick = function(){alert('hi')};
+
+})();

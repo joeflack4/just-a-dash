8dfcc6e76e7667e32f5e5f767a6f0c15c211abf5
joeflack4 <joeflack4@gmail.com>
2016-05-22 17:27:37 -0500

    Completed publishing of core API's, with embedded security permissions.

--- app/__init__.py
+++ app/__init__.py
@@ -81,40 +81,55 @@ contacts_api_blueprint = api_manager.create_api(Contacts, collection_name='conta
                                                 'PUT'],preprocessors=dict(GET_SINGLE = [ApiAuth.basic_admin],
                                                 GET_MANY = [ApiAuth.basic_admin]))
 
-# customers_api_blueprint = api_manager.create_api(Customers, collection_name='customers',
-#                                                  methods=['GET', 'POST', 'DELETE', 'PUT'],
-#                                                  preprocessors=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func]))
-# personnel_api_blueprint = api_manager.create_api(Personnel, collection_name='personnel',
-#                                                  methods=['GET', 'POST', 'DELETE', 'PUT'],
-#                                                  preprocessors=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func]))
+# - API's requiring basic CRM admin status.
+customers_api_blueprint = api_manager.create_api(Customers, collection_name='customers',
+                                                 methods=['GET', 'POST', 'DELETE', 'PUT'],
+                                                 preprocessors=dict(GET_SINGLE=[ApiAuth.basic_crm_admin],
+                                                 GET_MANY=[ApiAuth.basic_crm_admin]))
+agencies_api_blueprint = api_manager.create_api(Agencies, collection_name='agencies',
+                                                methods=['GET', 'POST', 'DELETE', 'PUT'],
+                                                preprocessors=dict(GET_SINGLE=[ApiAuth.basic_crm_admin],
+                                                                   GET_MANY=[ApiAuth.basic_crm_admin]))
 
-# result_api_blueprint = api_manager.create_api(Result, collection_name='keyword-analysis-results',
-#                                               methods=['GET', 'POST', 'DELETE', 'PUT'],
-#                                               preprocessors=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func]))
-# agencies_api_blueprint = api_manager.create_api(Agencies, collection_name='agencies',
-#                                                 methods=['GET', 'POST', 'DELETE', 'PUT'],
-#                                                 preprocessors=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func]))
-# modules_api_blueprint = api_manager.create_api(Modules, collection_name='modules',
-#                                                methods=['GET', 'POST', 'DELETE', 'PUT'],
-#                                                preprocessors=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func]))
+# - API's requiring basic HRM admin status.
+personnel_api_blueprint = api_manager.create_api(Personnel, collection_name='personnel',
+                                                 methods=['GET', 'POST', 'DELETE', 'PUT'],
+                                                 preprocessors=dict(GET_SINGLE=[ApiAuth.basic_hrm_admin], GET_MANY=[ApiAuth.basic_hrm_admin]))
+
+# - API's requiring basic MMS admin status.
+result_api_blueprint = api_manager.create_api(Result, collection_name='keyword-analysis-results',
+                                              methods=['GET', 'POST', 'DELETE', 'PUT'],
+                                              preprocessors=dict(GET_SINGLE=[ApiAuth.basic_mms_admin], GET_MANY=[ApiAuth.basic_mms_admin]))
 
 # - API's requiring super admin status.
-# app_notifications_api_blueprint = api_manager.create_api(AppNotifications, collection_name='app-notifications', methods=['GET', 'POST', 'DELETE', 'PUT'],
-#                                                 preprocessors=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func]))
-# roles_api_blueprint = api_manager.create_api(Roles, collection_name='user-roles', methods=['GET', 'POST', 'DELETE', 'PUT'],
-#                                                 preprocessors=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func]))
-# permissions_api_blueprint = api_manager.create_api(Permissions, collection_name='user-permissions', methods=['GET', 'POST', 'DELETE', 'PUT'],
-#                                                 preprocessors=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func]))
-# app_config_api_blueprint = api_manager.create_api(AppConfig, collection_name='app-config', methods=['GET', 'POST', 'DELETE', 'PUT'],
-#                                                 preprocessors=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func]))
-# crm_config_api_blueprint = api_manager.create_api(CrmConfig, collection_name='crm-config', methods=['GET', 'POST', 'DELETE', 'PUT'],
-#                                                 preprocessors=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func]))
-# hrm_config_api_blueprint = api_manager.create_api(HrmConfig, collection_name='hrm-config', methods=['GET', 'POST', 'DELETE', 'PUT'],
-#                                                 preprocessors=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func]))
-# oms_config_api_blueprint = api_manager.create_api(OmsConfig, collection_name='oms-config', methods=['GET', 'POST', 'DELETE', 'PUT'],
-#                                                 preprocessors=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func]))
-# mms_config_api_blueprint = api_manager.create_api(MmsConfig, collection_name='mms-config', methods=['GET', 'POST', 'DELETE', 'PUT'],
-#                                                 preprocessors=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func]))
+app_notifications_api_blueprint = api_manager.create_api(AppNotifications, collection_name='app-notifications',
+                                                         methods=['GET', 'POST', 'DELETE', 'PUT'],
+                                                         preprocessors=dict(GET_SINGLE=[ApiAuth.super_admin],
+                                                         GET_MANY=[ApiAuth.super_admin]))
+roles_api_blueprint = api_manager.create_api(Roles, collection_name='user-roles', methods=['GET', 'POST', 'DELETE',
+                                             'PUT'], preprocessors=dict(GET_SINGLE=[ApiAuth.super_admin],
+                                             GET_MANY=[ApiAuth.super_admin]))
+permissions_api_blueprint = api_manager.create_api(Permissions, collection_name='user-permissions', methods=['GET',
+                                                   'POST', 'DELETE', 'PUT'], preprocessors=dict(
+                                                    GET_SINGLE=[ApiAuth.super_admin], GET_MANY=[ApiAuth.super_admin]))
+modules_api_blueprint = api_manager.create_api(Modules, collection_name='modules', methods=['GET', 'POST', 'DELETE',
+                                               'PUT'], preprocessors=dict(GET_SINGLE=[ApiAuth.super_admin],
+                                                GET_MANY=[ApiAuth.super_admin]))
+app_config_api_blueprint = api_manager.create_api(AppConfig, collection_name='app-config', methods=['GET', 'POST',
+                                                  'DELETE', 'PUT'], preprocessors=dict(
+                                                  GET_SINGLE=[ApiAuth.super_admin], GET_MANY=[ApiAuth.super_admin]))
+crm_config_api_blueprint = api_manager.create_api(CrmConfig, collection_name='crm-config', methods=['GET', 'POST',
+                                                  'DELETE', 'PUT'], preprocessors=dict(
+                                                  GET_SINGLE=[ApiAuth.super_admin], GET_MANY=[ApiAuth.super_admin]))
+hrm_config_api_blueprint = api_manager.create_api(HrmConfig, collection_name='hrm-config', methods=['GET', 'POST',
+                                                  'DELETE', 'PUT'], preprocessors=dict(
+                                                  GET_SINGLE=[ApiAuth.super_admin], GET_MANY=[ApiAuth.super_admin]))
+oms_config_api_blueprint = api_manager.create_api(OmsConfig, collection_name='oms-config', methods=['GET', 'POST',
+                                                  'DELETE', 'PUT'], preprocessors=dict(
+                                                  GET_SINGLE=[ApiAuth.super_admin], GET_MANY=[ApiAuth.super_admin]))
+mms_config_api_blueprint = api_manager.create_api(MmsConfig, collection_name='mms-config', methods=['GET', 'POST',
+                                                  'DELETE', 'PUT'], preprocessors=dict(
+                                                  GET_SINGLE=[ApiAuth.super_admin], GET_MANY=[ApiAuth.super_admin]))
 
 ######################
 ### Initialize UI  ###
@@ -126,14 +141,15 @@ except:
     exceptions[1] = True
 
 if exceptions != {}:
-    if exceptions[1] == True:
+    if exceptions[1]:
         script_being_run = sys.argv[0]
         if script_being_run != 'manage.py':
             print('')
-            print('Exception #1. Main __init__.py attempted to set Secret Key value based on DB value, but an error occurred.'
-                  'This is likely due to one of the following reasons: (1) You are running db_create.py, or (2) You are '
-                  'attempting to run the application for the first time, but have not populated the database by running '
-                  'db_create.py. If (2), run db_create.py. If (1), this error is expected, and perfectly alright.')
+            print('Exception #1. Main __init__.py attempted to set Secret Key value based on DB value, but an error '
+                  'occurred. This is likely due to one of the following reasons: (1) You are running db_create.py, or '
+                  '(2) You are attempting to run the application for the first time, but have not populated the '
+                  'database by running db_create.py. If (2), run db_create.py. If (1), this error is expected, and '
+                  'perfectly alright.')
             print('')
 
 # - Contingencies
